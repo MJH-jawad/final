f03f6c80712563df94207f51e94052d17ea116f1
Md Hossein <md.hossein01@student.wandw.ac.nz>
2026-06-04 14:52:39 +0600

    Jawad

--- Hotel App/Room.cs
+++ Hotel App/Room.cs
@@ -6,7 +6,28 @@ using System.Threading.Tasks;
 
 namespace Hotel_App
 {
-    internal class Room
+    public class Room
     {
+        public int RoomNo;
+        public string RoomType;
+        public double Price;
+        public bool IsAvailable;
+
+        public Room(int roomNo, string roomType, double price)
+        {
+            RoomNo = roomNo;
+            RoomType = roomType;
+            Price = price;
+            IsAvailable = true;
+        }
+
+        public void DisplayRoom()
+        {
+            Console.WriteLine("---------------");
+            Console.WriteLine("Room No      : " + RoomNo);
+            Console.WriteLine("Room Type    : " + RoomType);
+            Console.WriteLine("Price/Night  : $" + Price);
+            Console.WriteLine("Available    : " + (IsAvailable ? "Yes" : "No"));
+        }
     }
 }
